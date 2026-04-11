3249985bd5c2fc8b6a93326d88535806f9ec64ef
Misha <misha.xomen888@gmail.com>
2026-04-11 12:06:30 +0300

    half of mission 5

---
+++ Assets/Course Library/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    
+    
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
